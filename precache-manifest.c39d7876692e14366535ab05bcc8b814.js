b06d092210922b1bde6e0ad40b75527c66474a8e
Fabius Con <adanet18@gmail.com>
2021-09-20 15:29:34 +0200

    Updates

---
+++ precache-manifest.c39d7876692e14366535ab05bcc8b814.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "f282c23720070f2767f5",
+    "url": "https://statistiche.direcas.com/static/js/0.3e10b296.chunk.js"
+  },
+  {
+    "revision": "5d274c6618025c94576c",
+    "url": "https://statistiche.direcas.com/static/js/1.74728835.chunk.js"
+  },
+  {
+    "revision": "ab7ca8716edbe60f1c1b",
+    "url": "https://statistiche.direcas.com/static/js/2.ba4f6d43.chunk.js"
+  },
+  {
+    "revision": "ced415e9c3ca7b05b2d0",
+    "url": "https://statistiche.direcas.com/static/js/3.97a6dfae.chunk.js"
+  },
+  {
+    "revision": "4947d938528fc6d0caf0",
+    "url": "https://statistiche.direcas.com/static/css/main.26c3079d.chunk.css"
+  },
+  {
+    "revision": "4947d938528fc6d0caf0",
+    "url": "https://statistiche.direcas.com/static/js/main.a87c7700.chunk.js"
+  },
+  {
+    "revision": "1b8fb14fcebc6ef3075e",
+    "url": "https://statistiche.direcas.com/static/js/runtime~main.999203f0.js"
+  },
+  {
+    "revision": "b15d4dcad1d823ec2f8f",
+    "url": "https://statistiche.direcas.com/static/js/6.167c2e21.chunk.js"
+  },
+  {
+    "revision": "ad9846d0b09268b63a7f",
+    "url": "https://statistiche.direcas.com/static/js/7.690045b2.chunk.js"
+  },
+  {
+    "revision": "786b90caae501e929980",
+    "url": "https://statistiche.direcas.com/static/js/8.1e7101bf.chunk.js"
+  },
+  {
+    "revision": "e9443f79097747a0deea",
+    "url": "https://statistiche.direcas.com/static/js/9.543fc682.chunk.js"
+  },
+  {
+    "revision": "ec2ccd01b87b679054a5",
+    "url": "https://statistiche.direcas.com/static/js/10.606bbace.chunk.js"
+  },
+  {
+    "revision": "9fa85f871b7ed40cee88",
+    "url": "https://statistiche.direcas.com/static/js/11.4bc7759c.chunk.js"
+  },
+  {
+    "revision": "73d99dd86b38f5bdd61d",
+    "url": "https://statistiche.direcas.com/static/js/12.17be9eb2.chunk.js"
+  },
+  {
+    "revision": "bc6970e4119254a61372",
+    "url": "https://statistiche.direcas.com/static/js/13.2680bfa6.chunk.js"
+  },
+  {
+    "revision": "20067393695d7c551fe0",
+    "url": "https://statistiche.direcas.com/static/js/14.e331f069.chunk.js"
+  },
+  {
+    "revision": "122cc875a037a2910f90",
+    "url": "https://statistiche.direcas.com/static/js/15.7dedc4be.chunk.js"
+  },
+  {
+    "revision": "13f822aee158e6a683dd",
+    "url": "https://statistiche.direcas.com/static/js/16.2c77787b.chunk.js"
+  },
+  {
+    "revision": "f16992ec5b3f04a021c5",
+    "url": "https://statistiche.direcas.com/static/js/17.592cff64.chunk.js"
+  },
+  {
+    "revision": "c1a936b43b298401efb6",
+    "url": "https://statistiche.direcas.com/static/js/18.cbb227b3.chunk.js"
+  },
+  {
+    "revision": "86bcb32cba0aeeb055f8",
+    "url": "https://statistiche.direcas.com/static/js/19.176ee9cb.chunk.js"
+  },
+  {
+    "revision": "8e73fcb7b96fcc5e637c",
+    "url": "https://statistiche.direcas.com/static/js/20.1b376df2.chunk.js"
+  },
+  {
+    "revision": "a8c85deff784c5157b8c",
+    "url": "https://statistiche.direcas.com/static/js/21.84bbfbe8.chunk.js"
+  },
+  {
+    "revision": "2964de2bb1c9d429a714",
+    "url": "https://statistiche.direcas.com/static/js/22.e0a8c7a8.chunk.js"
+  },
+  {
+    "revision": "6410090d5b16c58525fc",
+    "url": "https://statistiche.direcas.com/static/js/23.6c3e4990.chunk.js"
+  },
+  {
+    "revision": "264ed012a7786f5cb3bd",
+    "url": "https://statistiche.direcas.com/static/js/24.32dbf070.chunk.js"
+  },
+  {
+    "revision": "c096d258843228b2e2b9cf45a7542257",
+    "url": "https://statistiche.direcas.com/static/media/user-placeholder.c096d258.png"
+  },
+  {
+    "revision": "c54eef50718560d1268aa8e92025972d",
+    "url": "https://statistiche.direcas.com/static/media/background_1920-2.c54eef50.jpg"
+  },
+  {
+    "revision": "80d4a4e5af9b6cf3882e4a34c5497542",
+    "url": "https://statistiche.direcas.com/static/media/sidebar-4.80d4a4e5.jpg"
+  },
+  {
+    "revision": "85e70c0a01f21ea1413db13fe6b76af0",
+    "url": "https://statistiche.direcas.com/index.html"
+  }
+];
---
+++ static/js/24.32dbf070.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{444:function(e,t,a){"use strict";a.r(t);var n=a(58),s=a(1),c=a.n(s),r=a(197),i=a(198);t.default=function(){return c.a.createElement(r.a,{style:{height:"100vh",justifyContent:"center",alignItems:"center"}},c.a.createElement(i.a,{md:6,lg:4,className:"text-center"},c.a.createElement(n.a,{type:"display-1",className:"text-danger"},"404"),c.a.createElement("p",null,"La pagina che stai cercando non esiste o \xe8 stata rimossa.")))}}}]);
+//# sourceMappingURL=24.32dbf070.chunk.js.map
